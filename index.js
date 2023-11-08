adf66bbdeab4884ef23338c95047fbda28ea0bf7
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2023-11-08 15:33:23 +0600

    jwt token is  ruunning

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
+const jwt = require('jsonwebtoken')
+const cookieparser = require('cookie-parser')
 require('dotenv').config();
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
 app.use(express.json())
+app.use(cookieparser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8ywqwd.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,6 +33,19 @@ async function run() {
         await client.connect();
         const foodcollection = client.db('foodcollectiondb').collection("foods")
         const foodrequestcollection = client.db('foodcollectiondb').collection('requestcollection')
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log("token res", user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none'
+                })
+                .send({ success: true })
+        })
 
         app.get('/foods', async (req, res) => {
             const cursor = foodcollection.find();
@@ -64,7 +85,7 @@ async function run() {
             res.send(result)
         })
 
-        app.put('/requestfoods/:id', async (req, res) => {
+        app.put('/foods/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true };
@@ -83,6 +104,34 @@ async function run() {
                 }
             }
             const result = await foodcollection.updateOne(filter, updates, options)
+            res.send(result)
+        })
+
+        app.put('/requestfoods/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatestatus = req.body;
+            const updatesrequest = {
+                $set: {
+                    status: updatestatus.status
+                }
+            }
+            const result = await foodrequestcollection.updateOne(filter, updatesrequest, options)
+            res.send(result)
+        })
+
+        app.delete('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const result = await foodcollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        app.delete('/requestfoods/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const result = await foodrequestcollection.deleteOne(filter)
             res.send(result)
         })
 
